512c3c29ea3f7ec67e6f03fd2d7f3bbbeecd5bbd
tanphuccgl <phamnguyentanphucgl@gmail.com>
2021-07-06 17:18:07 +0700

    update2

--- lib/feature/profile/presentation/widgets/birthday_widget.dart
+++ lib/feature/profile/presentation/widgets/birthday_widget.dart
@@ -12,8 +12,6 @@ class DatePickerWidget extends StatefulWidget {
 }
 
 class _DatePickerWidgetState extends State<DatePickerWidget> {
-
-
   DateTime date;
   var formatter = new DateFormat('yyyy-dd-MM');
 
@@ -21,35 +19,32 @@ class _DatePickerWidgetState extends State<DatePickerWidget> {
     if (date == null) {
       return 'Select Date';
     } else {
-      widget.data.data.dateBirth=DateFormat('yyyy-dd-MM').format(date);
+      widget.data.data.dateBirth = DateFormat('yyyy-dd-MM').format(date);
       return DateFormat('yyyy-dd-MM').format(date);
       // return '${date.month}/${date.day}/${date.year}';
     }
   }
 
-@override
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    if(widget.data.data.dateBirth=="")
-      {
-print("Aaa");
-      }else
-        { print("bbb");
-          date=DateFormat("yyyy-dd-MM").parse(widget.data.data.dateBirth);
-
-
-        }
-
+    if (widget.data.data.dateBirth == "") {
+      print("Aaa");
+    } else {
+      print("bbb");
+      date = DateFormat("yyyy-dd-MM").parse(widget.data.data.dateBirth);
+    }
   }
+
   @override
   Widget build(BuildContext context) {
-
     return ButtonHeaderWidget(
-    title: '',
-    text: getText(),
-    onClicked: () => pickDate(context),
-  );}
+      title: '',
+      text: getText(),
+      onClicked: () => pickDate(context),
+    );
+  }
 
   Future pickDate(BuildContext context) async {
     final initialDate = DateTime.now();
@@ -65,6 +60,7 @@ print("Aaa");
     setState(() => date = newDate);
   }
 }
+
 class ButtonHeaderWidget extends StatelessWidget {
   final String title;
   final String text;
@@ -79,12 +75,12 @@ class ButtonHeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => HeaderWidget(
-    title: title,
-    child: ButtonWidget(
-      text: text,
-      onClicked: onClicked,
-    ),
-  );
+        title: title,
+        child: ButtonWidget(
+          text: text,
+          onClicked: onClicked,
+        ),
+      );
 }
 
 class ButtonWidget extends StatelessWidget {
@@ -99,8 +95,9 @@ class ButtonWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size= MediaQuery.of(context).size;
-    return Container(width:size.width/3.6 ,
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width / 3.6,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           minimumSize: Size.fromHeight(40),
@@ -130,18 +127,18 @@ class HeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        title,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      const SizedBox(height: 8),
-      child,
-    ],
-  );
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 8),
+          child,
+        ],
+      );
 }
